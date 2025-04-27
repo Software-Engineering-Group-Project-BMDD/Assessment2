2519254c8d42ec09f09cd0b2f2f680f77f6fc7c1
MiTsErRobot1943 <109624473+MiTsErRobot1943@users.noreply.github.com>
2025-04-27 20:37:42 +0100

    Revert "Add in a mock-class for if the sensors are connected "

--- DatabaseRepository.cs
+++ DatabaseRepository.cs
@@ -4,22 +4,11 @@ using Microsoft.Data.Sqlite;
 
 namespace MauiApp1
 {
-    /// <summary>
-    /// Provides methods for interacting with the database, including CRUD operations.
-    /// </summary>
     public static class DatabaseRepository
     {
-        /// <summary>
-        /// Adds a new user to the database.
-        /// </summary>
-        /// <param name="firstName">The first name of the user.</param>
-        /// <param name="lastName">The last name of the user.</param>
-        /// <param name="address">The address of the user.</param>
-        /// <param name="roleId">The role ID of the user.</param>
-        /// <returns>The ID of the newly added user.</returns>
-        public static int AddUser2Db(string firstName, string lastName, string address, int roleId)
+        public static int AddUser(string firstName, string lastName, string address, int roleId)
         {
-            var user_parameters = new Dictionary<string, object>
+            var parameters = new Dictionary<string, object>
             {
                 { "@f_name", firstName },
                 { "@l_name", lastName },
@@ -27,14 +16,10 @@ namespace MauiApp1
                 { "@role", roleId }
             };
 
-            ExecuteNonQuery("INSERT INTO Users (F_Name, L_Name, Address, Role) VALUES (@f_name, @l_name, @address, @role)", user_parameters);
+            ExecuteNonQuery("INSERT INTO Users (F_Name, L_Name, Address, Role) VALUES (@f_name, @l_name, @address, @role)", parameters);
             return Convert.ToInt32(ExecuteScalar("SELECT last_insert_rowid()"));
         }
 
-        /// <summary>
-        /// Retrieves all users along with their roles from the database.
-        /// </summary>
-        /// <returns>A <see cref="SqliteDataReader"/> containing the user and role data.</returns>
         public static SqliteDataReader GetUsersWithRoles()
         {
             return ExecuteReader(@"SELECT u.User_id, u.F_Name, u.L_Name, u.Address, r.Role_name 
@@ -42,12 +27,6 @@ namespace MauiApp1
                                    LEFT JOIN Role r ON u.Role = r.Role_Id");
         }
 
-        /// <summary>
-        /// Executes a non-query SQL command (e.g., INSERT, UPDATE, DELETE).
-        /// </summary>
-        /// <param name="commandText">The SQL command to execute.</param>
-        /// <param name="parameters">The parameters for the SQL command.</param>
-        /// <returns>The number of rows affected.</returns>
         private static int ExecuteNonQuery(string commandText, Dictionary<string, object> parameters = null)
         {
             using var connection = DatabaseConnectionManager.GetConnection();
@@ -64,12 +43,6 @@ namespace MauiApp1
             return command.ExecuteNonQuery();
         }
 
-        /// <summary>
-        /// Executes a SQL query and returns a <see cref="SqliteDataReader"/> for reading the results.
-        /// </summary>
-        /// <param name="commandText">The SQL query to execute.</param>
-        /// <param name="parameters">The parameters for the SQL query.</param>
-        /// <returns>A <see cref="SqliteDataReader"/> containing the query results.</returns>
         private static SqliteDataReader ExecuteReader(string commandText, Dictionary<string, object> parameters = null)
         {
             var connection = DatabaseConnectionManager.GetConnection();
@@ -86,13 +59,6 @@ namespace MauiApp1
             return command.ExecuteReader();
         }
 
-        /// <summary>
-        /// Executes a scalar SQL query and returns the first column of the first row in the result set.
-        /// </summary>
-        /// <param name="commandText">The SQL query to execute.</param>
-        /// <param name="parameters">The parameters for the SQL query.</param>
-        /// <returns>The result of the scalar query.</returns>
-        /// <exception cref="InvalidOperationException">Thrown if the scalar query returns null.</exception>
         private static object ExecuteScalar(string commandText, Dictionary<string, object> parameters = null)
         {
             using var connection = DatabaseConnectionManager.GetConnection();
@@ -106,8 +72,7 @@ namespace MauiApp1
                 }
             }
 
-            var result = command.ExecuteScalar();
-            return result ?? throw new InvalidOperationException("ExecuteScalar returned null.");
+            return command.ExecuteScalar();
         }
     }
 }
