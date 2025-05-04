5d17206edd676f3106bed0e904711b0512f09953
Declyn <me@declyn.dev>
2025-05-05 00:57:55 +0100

    add tests

--- MauiApp1.Tests/UnitTest1.cs
+++ MauiApp1.Tests/UnitTest1.cs
@@ -1,10 +1,45 @@
+using System.Threading.Tasks;
+using MauiApp1.UI.Model;
+using Microsoft.Maui.Platform;
+
 namespace MauiApp1.Tests;
 
 public class UnitTest1
 {
+
+    private SensorDatabase database = new SensorDatabase();
+
     [Fact]
-    public void Test1()
+    public void ValidateSensorModel()
     {
-        Assert.True(Constants.DatabaseFilename != null);
+        var sensor = new Sensor
+        {
+            Type = "Test",
+            Longitude = 5,
+            Latitude = 10
+        };
+
+        Assert.True(sensor.Longitude == 5 && sensor.Latitude == 10);
     }
+
+    [Fact]
+    public async Task ValidateDatabasePersists()
+    {
+        await database.Populate();
+        var sensors = await database.GetSensorsAsync();
+
+        Assert.True(sensors.Count() > 0);
+    }
+
+    [Fact]
+    public async Task ValidateBackup()
+    {
+        File.Delete(Constants.BackupPath);
+
+        await database.Populate();
+        await database.Backup();
+
+        Assert.True(File.Exists(Constants.BackupPath));
+    }
+
 }
